9cdd93ba51bb46ae6465ab8f3b066c07db97c51d
cmccoy0603 <cvmccoy123@gmail.com>
2024-10-04 15:31:11 -0500

    Added new view for logging in and creating user.

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "user"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("login/", views.login, name="login")
 ]
